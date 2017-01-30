20baa7c143fd081a6fd862f22cca02105be46264
JOHN LEE <nasky987@gmail.com>
2017-01-30 15:41:46 +0900

    # 스프링부트 시작하기(With Grails) - Grails3로 시작하기
    - /readingList으로 들어오는 요청을 처리하고, 해당 요청을 grails-app/views/readingList/index.gsp에 정의된 뷰로 포워드 한다.
    - 책 목록을 조회하여 모델에 추가하는 기능과
    - 책을 저장하는 기능을 하도록 컨트롤러를 수정하였다.

--- grails-app/controllers/spring_boot_with_grails3/ReadingListController.groovy
+++ grails-app/controllers/spring_boot_with_grails3/ReadingListController.groovy
@@ -1,6 +1,19 @@
 package spring_boot_with_grails3
 
+import grails.transaction.Transactional
+
 class ReadingListController {
 
-    def index() { }
+    def index() {
+        respond Book.list(params), model:[book: new Book()]
+    }
+
+    @Transactional
+    def save(Book book) {
+        book.reader = "hreeman"
+        
+        book.save flush:true
+
+        redirect(action: "index")
+    }
 }
